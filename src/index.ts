5ce3b22b1eacb97818a6b4e13f65652981a5a4a9
Paul David Tan <cryxuz@gmail.com>
2023-11-16 16:41:17 +1300

    initial

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://tailored-tails.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 200,
