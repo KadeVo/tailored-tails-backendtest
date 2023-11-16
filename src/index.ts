b25751e4961727fd10c294e139363b5f4eed838c
Kadin Vo <kadinvo@gmail.com>
2023-11-17 11:48:45 +1300

    Wildcard did not work

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://tailoredtails.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 200,
